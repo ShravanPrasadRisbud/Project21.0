ebef9884eb22157f8fbcc6d66f4803402417a8a5
ShravanPrasadRisbud <73066805+ShravanPrasadRisbud@users.noreply.github.com>
2020-10-25 14:20:39 +0530

    Add files via upload

---
+++ Project 21.js
@@ -0,0 +1,58 @@
+var thickness,wall;
+var bullet,speed,weight;
+
+function setup(){
+    createCanvas(1600,400);
+
+  speed=random(55,90);
+  weight=random(400,1500);
+  thickness=random(22,83);
+
+  bullet=createSprite(50,200,50,50);
+  bullet.velocityX = speed;
+  bullet.shapeColor=color(255);
+
+
+  wall=createSprite(1500,200,thickness,height/2);
+  wall.shapeColor=color(230,230,230);
+
+}
+
+
+function draw() {
+ background(0);
+
+//bullet.sprite.collide(wall.sprite,calculateDeformation)
+  if(hasCollided(bullet,wall))
+{
+  bullet.velocityX=0;
+  var damage=0.5 * weight * speed * speed/(thickness * thickness * thickness);
+
+  if(damage>10)
+  {
+    wall.shapeColor=color(255,0,0);
+  }
+
+  if(damage<10)
+  {
+    wall.shapeColor=color(0,255,0);
+  }
+
+}
+
+drawSprites();
+}
+
+function hasCollided(lbullet,lwall) {
+
+   bulletRightEdge=lbullet.X + lbullet.width;
+   wallLeftEdge=iwall.X;
+   if(bulletRightEdge>=wallLeftEdge){
+     return true;
+   }
+   return false;
+}
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Project21.0
-wall testing by gun
+# p5.play-boilerplate
+Boiler plate for p5.play
